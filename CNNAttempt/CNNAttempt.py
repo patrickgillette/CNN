680d1ebd6f6b6e0eb9d3e9518f341b276877efd7
patrickgillette <58642860+patrickgillette@users.noreply.github.com>
2026-03-18 13:06:24 -0500

    Clean up CNNAttempt.py by removing comment
    Removed unnecessary comment and adjusted import.

--- CNNAttempt/CNNAttempt.py
+++ CNNAttempt/CNNAttempt.py
@@ -12,8 +12,6 @@ import pathlib
 import TrainingClasses as tc
 from PIL import Image  # Add this import at the top of your file
 
-#Comment to save GIT
-
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
